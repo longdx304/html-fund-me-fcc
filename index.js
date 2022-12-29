6592d40ca3e4c084261f0135eb2b43d1e8ecad3c
Do Xuan Long <longdo@dobtech.vn>
2022-12-29 23:04:42 +0700

    added fund functionality

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import { ethers } from './ethers-5.2.esm.min.js';
+import { abi, contractAddress } from './constants.js';
+
+const connectButton = document.getElementById('connectButton');
+const fundButton = document.getElementById('fundButton');
+connectButton.onclick = connect;
+fundButton.onclick = fund;
+
 async function connect() {
   if (typeof window.ethereum != 'undefined') {
     try {
@@ -9,5 +17,26 @@ async function connect() {
   } else {
     document.getElementById('connectButton').innerHTML =
       'Please install metamask';
+  }
+}
+
+async function fund() {
+  const ethAmount = '1';
+  console.log(`Funding with ${ethAmount}...`);
+  if (typeof window.ethereum != 'undefined') {
+    // provider / connection to the blockchain
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // signer / wallet / someone with some gas
+    const signer = provider.getSigner();
+    // contract that we are interaction with (ABI & Address)
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+
+    try {
+      const transactionResponse = await contract.fund({
+        value: ethers.utils.parseEther(ethAmount),
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
